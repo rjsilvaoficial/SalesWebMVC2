70e57351f948f477a40a05ebad4940a2f634cc01
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-09-17 10:12:27 -0300

    Configurada comunicacao com banco MySql e efetuado primeiro migration

--- SalesWebMVC2/Startup.cs
+++ SalesWebMVC2/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMVC2
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMVC2Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVC2Context")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMVC2Context"), builder => 
+                    builder.MigrationsAssembly("SalesWebMVC2")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
