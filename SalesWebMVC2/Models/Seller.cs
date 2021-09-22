3ba3b34bd835ccb463a8b442d1acadc5be7c2c6e
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-09-22 09:03:29 -0300

    Foreign key not null referential integrity

--- SalesWebMVC2/Models/Seller.cs
+++ SalesWebMVC2/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWebMVC2.Models
         public DateTime BirthData { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
