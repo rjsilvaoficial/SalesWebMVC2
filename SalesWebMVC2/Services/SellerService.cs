eb0ad1ac685bb8343b1a6a6f98235e6c8562293a
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-09-24 09:58:23 -0300

    Referential integrity to delete added

--- SalesWebMVC2/Services/SellerService.cs
+++ SalesWebMVC2/Services/SellerService.cs
@@ -36,9 +36,17 @@ namespace SalesWebMVC2.Services
         
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
+
         }
 
         public async Task UpdateAsync(Seller obj)
