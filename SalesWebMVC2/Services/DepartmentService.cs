9965c1d6f25fa5f21b29ff9db76e3a09f126383f
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-09-24 09:47:28 -0300

    Asinc operations using Tasks

--- SalesWebMVC2/Services/DepartmentService.cs
+++ SalesWebMVC2/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC2.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC2.Data;
 using SalesWebMVC2.Models;
 using System;
 using System.Collections.Generic;
@@ -16,9 +17,9 @@ namespace SalesWebMVC2.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(department => department.Name).ToList();
+            return await _context.Department.OrderBy(department => department.Name).ToListAsync();
         }
     }
 }
